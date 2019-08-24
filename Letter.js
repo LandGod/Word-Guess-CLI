fd9022e8b88ce56bd135a2c7ed36ee3b4dd91215
Dan Gold <tentativechaos@gmail.com>
2019-08-24 12:05:34 -0700

    Rewrote class defentition for Letter to be more inline with best practice for creating an ES6 class. (Removed methods from constructor function.)

--- Letter.js
+++ Letter.js
@@ -1,29 +1,29 @@
 class Letter {
     constructor(theLetter) {
         if (theLetter.length > 1 || theLetter.length < 1) {
-            throw("Initial value for Letter object must be a single letter.");
+            throw ("Initial value for Letter object must be a single letter.");
         }
         this.value = theLetter.toLowerCase();
         this.wasGuessed = false;
-        this.display = function() {
-            if (this.wasGuessed) {
-                return this.value;
-            } else {
-                return '_';
-            };
-        };
-        this.check = function(guess) {
-            if (guess.toLowerCase() === this.value) {
-                this.wasGuessed = true;
-                return true;
-            } else {
-                return false;
-            }
-        }
+    }
 
+    display() {
+        if (this.wasGuessed) {
+            return this.value;
+        } else {
+            return '_';
+        };
+    };
+    check(guess) {
+        if (guess.toLowerCase() === this.value) {
+            this.wasGuessed = true;
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
 module.exports = {
     Letter: Letter
-  };
+};
