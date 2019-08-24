d25281cbe95157d744ea4ee02f6e7fe39e2d1203
Dan Gold <tentativechaos@gmail.com>
2019-08-24 14:31:01 -0700

    Created Word class in Word.js with export statement.

---
+++ Word.js
@@ -0,0 +1,67 @@
+const LetterFile = require("./Letter.js");
+const Letter = LetterFile.Letter
+
+class BadGuessLength extends Error {
+    constructor(...args) {
+        super(...args)
+        Error.captureStackTrace(this, BadGuessLength)
+    }
+}
+
+class Word {
+    constructor(newWord) {
+        this.answer = newWord.trim().toLowerCase()
+        this.__rawWrod = newWord.trim().split('')
+        this.word = []
+        for (let i = 0; i < this.__rawWrod.length; i++) {
+            this.word[i] = new Letter(this.__rawWrod[i]);
+        }
+    }
+
+    guess(userGuess) {
+        userGuess = userGuess.trim().toLowerCase()
+
+        // Single Letter Guess
+        // Perform check on each letter, return true if any number of letters returned true for the guess
+        if (userGuess.length === 1) {
+            let success = false;
+            for (let singleLetter in this.word) {
+                if (this.word[singleLetter].check(userGuess)) {
+                    success = true;
+                }
+            }
+
+            return success;
+
+        }
+        // Whole word guess
+        // Check user guess against answer, if true, set all letters to true then return true
+        else if (userGuess.length === this.answer.length) {
+            userGuess = userGuess.trim().toLowerCase();
+            if (userGuess === this.answer) {
+                for (let i in this.word) {
+                    this.word[i].wasGuessed = true;
+                }
+                return true;
+            }
+            return false;
+        }
+        // Junk guess
+        else {
+            throw(BadGuessLength('You may only guess either a single letter or the entire word at one time.'))
+        }
+    }
+
+    printWord() {
+        let output = [];
+        for (let i = 0; i < this.word.length; i++) {
+            output.push(this.word[i].display())
+        }
+        return output.join(' ');
+    }
+
+}
+
+module.exports = {
+    Word = Word
+};
